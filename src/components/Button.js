a09a95debe47d0def6b8d2ee4a27b35712c0fdc0
8Bts <mcmizze@yahoo.com>
2021-05-19 20:09:33 +0400

    Restore `public` and `build`, return `<button>` element in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Button = (props) => props.name;
+const Button = ({ name }) => <button type="button">{name}</button>;
 
 Button.propTypes = { name: PropTypes.string.isRequired };
 

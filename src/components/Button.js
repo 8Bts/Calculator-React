400a8795fb6de62704613ccbbeaf2b4e0877b52f
8Bts <mcmizze@yahoo.com>
2021-05-27 18:09:04 +0400

    Add listener for each button elements click event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => <button type="button">{name}</button>;
+const Button = ({ name, clickHandler }) => {
+  const handleCLick = () => {
+    clickHandler(name);
+  };
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+  return (
+    <button type="button" onClick={handleCLick}>{name}</button>
+  );
+};
+
+Button.propTypes = { name: PropTypes.string.isRequired, clickHandler: PropTypes.func.isRequired };
 
 export default Button;
